0b977e44813964a3bb9822052ff17acbf16ecbaf
Bharat <hibharatj@gmail.com>
2025-06-16 16:50:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function firstWord(s) {
   // your code here
+	if(s.length===0 || s.trim() === undefined) {
+		return ``;
+	}
+	let res = [];
+
+	let ans = s.split(" ");
+	return `${ans[0]}`;
 }
 
 // Do not change the code below
